68e94dc7c796af80244f7811d63937696000d1ba
ggrothendieck <ggrothendieck@e4389bc6-14ff-11de-82b5-194b17410922>
2009-03-20 04:00:07 +0000

    initial commit

---
+++ R/bc.R
@@ -0,0 +1,41 @@
+
+bc <- function(..., scale=100, logical=FALSE, cmd = "bc -l") {
+    if (missing(cmd)) {
+        cmd2 <- if (.Platform$OS.type == "windows") {
+            paste("cmd /c set BC_LINE_LENGTH=", 2*scale, "&&", cmd, sep = "")
+        } else paste("scale=", 2*scale, "  | bc")
+            paste("BC_LINE_LENGTH=", 2*scale, " | ", cmd, sep = "")
+    }
+    input <- c(paste("scale", scale, sep = "="), paste(..., sep = ""))
+    structure(system(cmd2, input = input, intern = TRUE), 
+		class = c("bc", "character"))
+    # if (logical) as.logical(result) else result
+}
+
+# - A bc object is a character string with class "bc".
+# - The ordinary R operators: +, -, etc. can operate on bc objects
+# giving new bc objects.
+# - printing a bc object causes it to be passes to bc and back
+
+as.character.bc <- function(x, ...) as.character(unclass(x))
+as.expression.bc <- function(x, ...) bc(x, ...)[[1]]
+
+as.bc <- function(x, ...) UseMethod("as.bc")
+
+Ops.bc <- function (e1, e2) 
+    if (missing(e2)) { bc(.Generic, e1)
+    } else bc(e1, .Generic, e2)
+
+transtab <- matrix( c("exp", "e", 
+					  "log", "l", 
+					  "sin", "s",
+					  "cos", "c"), nc = 2, byrow = TRUE)
+
+Math.bc <- function(x, ...) {
+	idx <- match(.Generic, transtab[,1], nomatch = 0)
+	fn <- if (idx > 0) transtab[idx, 2] else .Generic
+	bc(fn, "(", x, ")")
+}
+
+print.bc <- function(x, ...) print(unclass(bc(unclass(x), ...)))
+
