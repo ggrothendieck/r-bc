49fe08ebf43a3c990695b8f6f1b0fd34a4da6108
ggrothendieck <ggrothendieck@e4389bc6-14ff-11de-82b5-194b17410922>
2009-03-20 04:00:07 +0000

    initial commit

---
+++ trunk/R/bcInstall.R
@@ -0,0 +1,37 @@
+
+bcFile <- function(filename = "bc.exe",
+   slash = c("\\", "/")) {
+   stopifnot(.Platform$OS.type == "windows")
+   whole.path <- function(path, defpath, deffile) {
+      if (path == "") path <- defpath
+      if (file.info(path)$isdir) 
+         file.path(sub("[/\\]$", "", path), deffile) 
+      else path
+   }
+   bc.exe <- whole.path(Sys.getenv("BC_HOME"),
+         system.file(package = "bc", "bcdir"), "bc.exe")
+   fullname <- switch(match.arg(filename), bc.exe = bc.exe,
+	 system.file(package = "bc", "bcdir"))
+   slash <- match.arg(slash)
+   chartr(setdiff(c("/", "\\"), slash), slash, fullname)
+}
+
+bcInstall <- function(url = 
+   "http://r-bc.googlecode.com/files/bc.exe",
+   overwrite = FALSE) {
+   stopifnot(.Platform$OS.type == "windows")
+   tmpd <- tempdir()
+   tmpz <- file.path(tmpd, basename(url))
+   download.file(url, tmpz, mode = "wb")
+   files <- "bc.exe"
+   lf <- function(f) list.files(path = tmpd, pattern = f,
+      all.files = FALSE, full.names = TRUE, recursive = TRUE)
+   for (f in files)
+      if (length(lf(f)) ==0) stop(paste(f, "not found in", url))
+   for (f in files) if (!file.copy(lf(f), bcFile(f), overwrite = overwrite))
+      warning(paste(bcFile(f), "already exists.\n  Use overwrite = TRUE"))
+   invisible()
+}
+
+
+
---
+++ trunk/R/zzz.R
@@ -0,0 +1,10 @@
+
+.First.lib <- function(lib, pkg, ...) {
+   if (.Platform$OS.type != "windows") return()
+
+   if (!file.exists(bcFile("bc.exe"))) {
+	cat(bcFile("bc.exe"), "not found.\n")
+    cat("Run bcInstall() without arguments to install bc.\n")
+   }
+   invisible()
+}
