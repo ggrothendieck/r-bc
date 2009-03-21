61f30eda4791e3138872b49ec0487833b4dc3a22
ggrothendieck <ggrothendieck@e4389bc6-14ff-11de-82b5-194b17410922>
2009-03-21 00:02:32 +0000

    fixed bcInstall, better cmd processing in bc.

--- trunk/R/bc.R
+++ trunk/R/bc.R
@@ -1,18 +1,24 @@
-bc <- function(
-        ...,                           # input to bc
-        scale = getOption("bc.scale"), # precision
-        logical = FALSE,               # should the result be logical?
-        cmd = "bc -l") {               # the actual command to call bc
-    if (is.null(scale)) 
-        scale <- 100
-    out <- system(cmd, intern = TRUE,
-                  input = c(sprintf("scale=%d", scale), 
-                            paste(..., sep = "")))
-    if (nchar(Sys.getenv("BC_LINE_LENGTH")) == 0)
-        out <- paste(sub("\\\\", "", out), collapse = "")
-    if (logical) 
-        as.logical(out) 
-        else structure(out, class = c("bc", "character"))
+
+bc <- function(..., scale = getOption("bc.scale"), logical = FALSE, 
+	cmd, args = "-l") {
+	if (missing(cmd)) {
+		bcfile <- Sys.which("bc")
+		cmd <- if (nchar(bcfile) > 0) "bc"
+		else {
+			if (.Platform$OS.type == "windows") {
+				system.file("bcdir", "bc.exe", package = "bc")
+			} else system.file("bcdir", "bc", package = "bc")
+		}
+	}
+	cmd <- paste(cmd, args)
+	if (is.null(scale)) scale <- 100
+    out <- system(cmd, input = c(paste("scale", scale, sep = "="), 
+		paste(..., sep = "")), intern = TRUE)
+	if (nchar(Sys.getenv("BC_LINE_LENGTH")) == 0) {
+		out <- paste(sub("\\\\", "", out), collapse= "")
+	}
+	result <- structure(out, class = c("bc", "character"))
+    if (logical) as.logical(result) else result
 }
 
 # - A bc object is a character string with class "bc".
@@ -20,32 +26,22 @@ bc <- function(
 # giving new bc objects.
 # - printing a bc object causes it to be passes to bc and back
 
-as.character.bc <- function(x, ...) 
-	as.character(x)
-	
-# [wk] ??
-as.expression.bc <- function(x, ...) 
-	bc(x, ...)[[1]]
-	
-# [wk] TODO -- fix this one
-as.bc <- function(x, ...) 
-	UseMethod("as.bc")
+as.character.bc <- function(x, ...) as.character(unclass(x))
 
-Math.bc <- local({
-    transtab <- matrix( c("exp", "e", 
-                          "log", "l", 
-                          "sin", "s",
-                          "cos", "c"), nc = 2, byrow = TRUE)
-    function(x, ...) {
-    	idx <- match(.Generic, transtab[,1], nomatch = 0)
-	    fn <- if (idx > 0) transtab[idx, 2] 
-	          else .Generic
-	    bc(fn, "(", x, ")") } 
-})
+Ops.bc <- function (e1, e2) 
+    if (missing(e2)) { bc(.Generic, e1)
+    } else bc(e1, .Generic, e2)
 
-Ops.bc <- function (e1, e2) {
-    if (missing(e2)) bc(.Generic, e1)
-    else bc(e1, .Generic, e2) }
+transtab <- matrix( c("exp", "e", 
+					  "log", "l", 
+					  "sin", "s",
+					  "cos", "c"), nc = 2, byrow = TRUE)
 
-print.bc <- function(x, ...) 
-	print(unclass(bc(unclass(x), ...)))
+Math.bc <- function(x, ...) {
+	idx <- match(.Generic, transtab[,1], nomatch = 0)
+	fn <- if (idx > 0) transtab[idx, 2] else .Generic
+	bc(fn, "(", x, ")")
+}
+
+print.bc <- function(x, ...) print(unclass(bc(unclass(x), ...)))
+
