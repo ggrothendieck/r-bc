c3353ab8de47b706b7f72843e8974075eb541fed
ggrothendieck <ggrothendieck@e4389bc6-14ff-11de-82b5-194b17410922>
2009-03-23 22:14:14 +0000

    fixed zzz.R

--- trunk/R/zzz.R
+++ trunk/R/zzz.R
@@ -2,7 +2,7 @@
 .First.lib <- function(lib, pkg, ...) {
    if (.Platform$OS.type != "windows") return()
 
-   if (!file.exists(bcFile("bc.exe"))) {
+   if (is.null(Sys.which("bc.exe")) && !file.exists(bcFile("bc.exe"))) {
 	cat(bcFile("bc.exe"), "not found.\n")
     cat("Run bcInstall() without arguments to install bc.\n")
    }
